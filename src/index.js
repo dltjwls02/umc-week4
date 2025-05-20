ecf72fd13aa7d78e6dacf01bff3f0e1bc1472a54
emily9821 <102820056+dltjwls02@users.noreply.github.com>
2025-05-20 14:30:27 +0900

    swagger 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import dotenv from "dotenv";
 import express from "express";          // -> ES Module
 import cors from "cors";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
+
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleAddReview } from "./controllers/review.controller.js";
 import { handleAddMission } from "./controllers/mission.controller.js";
@@ -28,3 +31,34 @@ app.post("/api/mission/:missionId/challenge",handleChallengeMission);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+pp.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "UMC 7th",
+      description: "UMC 7th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
